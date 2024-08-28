d9d5d4396baec0d4aa3896217344287ba5fea425
Yassine AIT TOUIJAR <aittouijar.yassine@gmail.com>
2024-08-28 08:41:30 +0100

    all models update

--- app/Http/Livewire/AllModelsPage.php
+++ app/Http/Livewire/AllModelsPage.php
@@ -7,11 +7,11 @@ use Lunar\Models\Product;
 
 class AllModelsPage extends Component
 {
-    public $amount = 10;
+    public $amount = 20;
 
 
     public function loadMore(){
-        $this->amount += 10;
+        $this->amount += 20;
     }
 
 
@@ -19,7 +19,7 @@ class AllModelsPage extends Component
     {
         $products = Product::whereHas('collections', function($query){
             $query->where('lunar_collections.id', '<>' , 6);
-        })->where('status', 'published')->take($this->amount)->get();
+        })->where('status', 'published')->take($this->amount)->orderBy('created_at', 'desc')->get();
         return view('livewire.all_models', ['products' => $products])->section('slot');
     }
 }
