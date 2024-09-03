8fe567d4b46a07fb6db3fbf820cc0cf77db84ccb
Yassine AIT TOUIJAR <aittouijar.yassine@gmail.com>
2024-09-03 08:45:04 +0100

    show cubre in compenete

--- app/Http/Livewire/ComponentGardPage.php
+++ app/Http/Livewire/ComponentGardPage.php
@@ -19,7 +19,7 @@ class ComponentGardPage extends Component
     {
 
         $products = Product::whereHas('collections', function($query){
-            $query->where('lunar_collections.id', 7); // get puertas collection
+            $query->whereIn('lunar_collections.id', [7, 8]); //pilares - cubre pilar
         })->get();
         return view('livewire.component_gard', ['products' => $products]);
     }
