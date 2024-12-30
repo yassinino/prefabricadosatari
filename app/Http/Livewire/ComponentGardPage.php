a0bd065f99e673125f864d26cb77f99c8ff42045
Yassine AIT TOUIJAR <aittouijar.yassine@gmail.com>
2024-12-30 11:51:51 +0100

    addToFavorite Gard

--- app/Http/Livewire/ComponentGardPage.php
+++ app/Http/Livewire/ComponentGardPage.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Lunar\Models\Product;
+use App\Models\Wishlist;
+use Auth;
 
 class ComponentGardPage extends Component
 {
@@ -14,6 +16,28 @@ class ComponentGardPage extends Component
         $this->amount += 10;
     }
 
+
+    public function addToFavorite($product_id){
+        if(Auth::check()){
+
+            if(Wishlist::where('user_id', Auth()->user()->id)->where('product_id', $product_id)->first()){
+
+                Wishlist::where('user_id', Auth()->user()->id)->where('product_id', $product_id)->delete();
+
+            }else{
+
+                Wishlist::create([
+                    'user_id' => Auth()->user()->id,
+                    'product_id' => $product_id,
+                ]);
+
+                
+            }
+            
+        }else{
+            return redirect()->to('/login');
+        }
+    }
 
     public function render()
     {
